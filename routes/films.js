c043ad0c18a5cc3eaf8b723645773484f3be7a43
Rob Levy <robert.g.levy@gmail.com>
2018-03-09 16:37:39 +0000

    Simple WHERE clause added

--- routes/films.js
+++ routes/films.js
@@ -6,11 +6,15 @@ router.get('/:id/recommendations', getFilmRecommendations);
 
 function getFilmRecommendations(req, res, next) {
   var id = req.params.id;
+  console.log(id);
   models.Films.findOne({
     attributes: ['title'],
-    limit: 2
+    where: {
+        id: id
+    }
   }).then(film => {
-    var message = film.get({plain: true}).title;
+    //var message = film.get({plain: true}).title;
+    message = film;
     res.status(200).json(message);
   });
 }
